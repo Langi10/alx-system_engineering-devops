2b0e7a487544e11af8dfb2cee6c6ad4c43273fa6
Langi10 <abdulrahman.omboke@gmail.com>
2024-06-18 22:02:07 +0300

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,18 @@
+#!/usr/bin/python3
+"""function that queries the Reddit API and returns the
+number of subscribers for a given subreddit"""
+
 import requests
 
-def number_of_subscribers(subreddit):
-    # Define the URL for the subreddit's about page
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    
-    # Define the headers with a custom User-Agent
-    headers = {
-        "User-Agent": "python:subreddit.subscriber.count:v1.0 (by /u/yourusername)"
-    }
-    
-    try:
-        # Make a GET request to the subreddit's about page
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        # Check if the request was successful
-        if response.status_code == 200:
-            # Parse the JSON response
-            data = response.json()
-            # Return the number of subscribers
-            return data["data"]["subscribers"]
-        else:
-            # If the subreddit does not exist, return 0
-            return 0
-    except requests.RequestException:
-        # In case of any network errors, return 0
-        return 0
 
+def number_of_subscribers(subreddit):
+    """function that queries the Reddit API and returns the number of
+    subscribers for a given subreddit"""
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data').get('subscribers')
+    else:
+        return 0
